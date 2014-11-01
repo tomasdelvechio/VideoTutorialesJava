0cefc9cd3d03094fd054110f0f4ec9496bb24120
Tomas <tomasdelvechio17@gmail.com>
2014-11-01 20:50:26 -0300

    Bugfix en classname

--- src/PrimerPrograma/Multiplo.java
+++ src/PrimerPrograma/Multiplo.java
@@ -2,7 +2,7 @@ package PrimerPrograma;
 
 import static java.lang.System.out;
 
-class Multiplos {
+class Multiplo {
     public static void main(String[] args) {
         for (int x = 1; x <= 200; x++) {
             int multiplo = 7 * x;
